25b45644b644e6fa6f30bc99ceb7f4a437480ef9
Kleberson Canuto <klebersoncanuto@gmail.com>
2025-01-17 17:15:04 -0300

    Add unit tests

--- ultraocr/src/test/java/com/nuveo/ultraocr/ClientTest.java
+++ ultraocr/src/test/java/com/nuveo/ultraocr/ClientTest.java
@@ -1,14 +1,36 @@
 package com.nuveo.ultraocr;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 
-import org.junit.Test;
+import com.nuveo.ultraocr.exceptions.*;
 
-public class ClientTest 
-{
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+
+class ClientTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void shouldAuthenticate() throws IOException, InterruptedException {
+        HttpResponse mockResponse = Mockito.mock(HttpResponse.class);
+        Mockito.when(mockResponse.statusCode()).thenReturn(200);
+        Mockito.when(mockResponse.body()).thenReturn("{\"token\":\"123\"}");
+        HttpClient httpClient = Mockito.mock(HttpClient.class);
+        Mockito.when(httpClient.send(Mockito.any(), Mockito.any())).thenReturn(mockResponse);
+        Client client = new Client(httpClient);
+        assertDoesNotThrow(() -> client.authenticate("123", "123", 60));
+    }
+
+    @Test
+    void shouldFailToAuthenticate() throws IOException, InterruptedException {
+        HttpResponse mockResponse = Mockito.mock(HttpResponse.class);
+        Mockito.when(mockResponse.statusCode()).thenReturn(400);
+        HttpClient httpClient = Mockito.mock(HttpClient.class);
+        Mockito.when(httpClient.send(Mockito.any(), Mockito.any())).thenReturn(mockResponse);
+        Client client = new Client(httpClient);
+        assertThrows(InvalidStatusCodeException.class, () -> client.authenticate("123", "123", 60));
     }
 }
